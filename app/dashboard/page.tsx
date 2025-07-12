11b666a8587d8642b4b8e00361fddaf2f752d50d
michael-ola <michael303.mi@gmail.com>
2025-07-12 04:32:30 +0100

    built dasboard control panel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import AssetComponent from "@/components/dashboard/assets/AssetComponent";
+import ControlPanel from "@/components/dashboard/control-panel";
 export default function AssetModalPage() {
   return (
-    <div className="relative min-h-screen bg-[#0d181c] text-white">
+    <div className="relative min-h-screen bg-[#0d181c] text-white max-sm:flex max-sm:flex-col max-sm:items-center">
       <AssetComponent />
+      <ControlPanel />
     </div>
   );
 }
