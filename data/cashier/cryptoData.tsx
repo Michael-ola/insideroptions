882a882110eeb093f89a206758a94f03b221b75c
pinnoche <ptime552@gmail.com>
2025-07-14 16:30:50 +0100

    cryptoData changes

--- data/cashier/cryptoData.tsx
+++ data/cashier/cryptoData.tsx
@@ -1,8 +1,10 @@
 import qrCode from "@/lib/assets/QR_code.png";
-export const cryptoData = {
+export const cryptos = {
   label: "USDT (ERC20)",
   network: "ERC20",
   address: "0xEb3D10Cbf9820b2210D8dd544Be2D84f552342e4",
+  redeemScript:
+    "5221022a25d2ca604db97695c5fc235bd8b57608a6a5075779082e47e1b4c8c4a59e8521025d27813a2356ade150bbac0502061d504ad807c5672fc66e3292a8156651818a21029f87c37c3eecd0f58e08b731faadb55b4108d282734ed397a9e206138d8cdac853ae",
   qrCode: qrCode,
   instructions:
     "Once you initiate the transaction in your crypto wallet, we will email you confirmation of the pending transaction. The transaction usually takes up to 4 hour to be processed. You can track the progress using the blockchain explorer.",
