bb9130a2617d6315b9bf608bb285cd4351d8aaa5
michael-ola <michael303.mi@gmail.com>
2025-08-01 17:35:41 +0100

    changed auto trade button bottom spacing

--- components/dashboard/AutoTradeButton/AutoTradeButton.tsx
+++ components/dashboard/AutoTradeButton/AutoTradeButton.tsx
@@ -12,7 +12,7 @@ const AutoTradeButton = () => {
         setOpenAutoTrade(true);
         setShowTradeStatus(true);
       }}
-      className="fixed bottom-[19%] max-sm:bottom-[190px] z-5 left-1/2 max-sm:mx-auto -translate-x-1/2 text-center bg-transparent sm:hover:bg-primary/5 rounded-xl border border-primary/60 text-white shadow-lg py-2 px-8 max-sm:px-6 cursor-pointer"
+      className="fixed bottom-[115px] max-sm:bottom-[190px] z-5 left-1/2 max-sm:mx-auto -translate-x-1/2 text-center bg-transparent sm:hover:bg-primary/5 rounded-xl border border-primary/60 text-white shadow-lg py-2 px-8 max-sm:px-6 cursor-pointer"
     >
       View Auto Trade
       <div className="absolute w-2 h-2 p-2 -top-2 z-5 right-[10%] text-xs rounded-full bg-red-800 flex items-center justify-center">
