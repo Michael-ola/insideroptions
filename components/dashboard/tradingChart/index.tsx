0866ce97795015903ef59b2d4ea2d869762a1917
Ibinabo <billeibinabo@gmail.com>
2025-08-12 23:51:10 +0100

    Chart fixes

--- components/dashboard/tradingChart/index.tsx
+++ components/dashboard/tradingChart/index.tsx
@@ -8,7 +8,8 @@ const TradingChart: React.FC = () => {
     const chartRef = useRef<ReturnType<typeof createChart> | null>(null);
     const seriesRef = useRef<ISeriesApi<any> | null>(null);
     const { chartStyle, assetId } = useDashboardContext();
-    
+    // const [latestCoord, setLatestCoord] = React.useState<{ x: number; y: number } | null>(null);
+
     useEffect(() => {
         if (!containerRef.current) return;
 
